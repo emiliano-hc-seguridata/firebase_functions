f4dd91c4ca3b8eaf668b0934be12b36893f8b5d5
sofia <sofia>
2021-09-29 11:08:09 -0500

    newfunc

--- index.js
+++ index.js
@@ -15,77 +15,99 @@ const TEMPLATE_ID = functions.config().sendgrid.template;
 sgMail.setApiKey(API_KEY);
 
 exports.welcomeEmail = functions.auth.user().onCreate((user) => {
-	const msg = {
-		to: user.email,
-		from: "devops@seguridata.com",
-		template_id: TEMPLATE_ID,
-		dynamic_template_data: {
-			subject: user.displayName,
-			text: "Bienvenido",
-		},
-	};
-	return sgMail.send(msg);
+  const msg = {
+    to: user.email,
+    from: "devops@seguridata.com",
+    template_id: TEMPLATE_ID,
+    dynamic_template_data: {
+      subject: user.displayName,
+      text: "Bienvenido",
+    },
+  };
+  return sgMail.send(msg);
 });
 
 exports.signedDocEmail = functions.https.onCall(async (data, context) => {
-	if (!context.auth && !context.auth.token.email) {
-		throw new functions.https.HttpsError("failed-precondition",
-			"Must be logged in");
-	}
+  if (!context.auth && !context.auth.token.email) {
+    throw new functions.https.HttpsError(
+      "failed-precondition",
+      "Must be logged in"
+    );
+  }
 
-	const msg = {
-		to: context.auth.token.email,
-		from: "devops@seguridata.com",
-		template_id: TEMPLATE_ID,
-		dynamic_template_data: {
-			subject: context.auth.token.email,
-			text: "Documento firmado",
-		},
-	};
-	await sgMail.send(msg);
+  const msg = {
+    to: context.auth.token.email,
+    from: "devops@seguridata.com",
+    template_id: TEMPLATE_ID,
+    dynamic_template_data: {
+      subject: context.auth.token.email,
+      text: "Documento firmado",
+    },
+  };
+  await sgMail.send(msg);
 
-	return { success: true };
+  return { success: true };
 });
 
-exports.scheduledAlert = functions.pubsub.schedule("0 12 * * *").onRun(async (context) => {
-	const nowDate = new Date();
-	const snapshot = await firebase.firestore().collection("alertas").get();
-	snapshot.docs.forEach(alarm => {
-		let sendEmail = alarm.type === "daily";
+exports.uploadNewDoc = functions.https.onCall(async (data, context) => {
+  if (!context.auth && !context.auth.token.email) {
+    throw new functions.https.HttpsError(
+      "failed-precondition",
+      "Must be logged in"
+    );
+  }
 
-		if (alarm.type === "weekly") {
-			let daysDifference;
-			const elapsedTime = (nowDate.getTime() - alarm.time);
-			daysDifference = Math.floor(elapsedTime / 1000 / 60 / 60 / 24);
-			sendEmail = daysDifference === 7;
-		}
-		console.log(sendEmail, nowDate);
+  const text = `Sube un nuevo documento:${data}`;
 
-		if (sendEmail) {
-			alarm.emails.forEach(async (email) => {
-				const msg = {
-					to: email,
-					from: "devops@seguridata.com",
-					template_id: TEMPLATE_ID,
-					dynamic_template_data: {
-						subject: context.auth.token.email,
-						text: alarm.message,
-					},
-				};
-				await sgMail.send(msg);
-			})
-		}
+  const msg = {
+    to: context.auth.token.email,
+    from: "devops@seguridata.com",
+    template_id: TEMPLATE_ID,
+    dynamic_template_data: {
+      subject: context.auth.token.email,
+      text,
+    },
+  };
+  await sgMail.send(msg);
 
-	});
-	return null;
+  return { success: true };
 });
 
-exports.addAlert = functions.https.onCall((data, context) => {
+exports.scheduledAlert = functions.pubsub
+  .schedule("0 12 * * *")
+  .onRun(async (context) => {
+    const nowDate = new Date();
+    const snapshot = await firebase.firestore().collection("alertas").get();
+    snapshot.docs.forEach((alarm) => {
+      let sendEmail = alarm.type === "daily";
 
-});
+      if (alarm.type === "weekly") {
+        let daysDifference;
+        const elapsedTime = nowDate.getTime() - alarm.time;
+        daysDifference = Math.floor(elapsedTime / 1000 / 60 / 60 / 24);
+        sendEmail = daysDifference === 7;
+      }
+      console.log(sendEmail, nowDate);
 
+      if (sendEmail) {
+        alarm.emails.forEach(async (email) => {
+          const msg = {
+            to: email,
+            from: "devops@seguridata.com",
+            template_id: TEMPLATE_ID,
+            dynamic_template_data: {
+              subject: context.auth.token.email,
+              text: alarm.message,
+            },
+          };
+          await sgMail.send(msg);
+        });
+      }
+    });
+    return null;
+  });
 
-
+exports.addAlert = functions.https.onCall((data, context) => {});
 
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
